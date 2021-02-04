e9941839bc8eb171efbc30d2d45439603ae5b12b
sanketr62 <sanketr62@gmail.com>
2021-02-04 20:12:45 +0530

    update 2.0

---
+++ myScript.js
@@ -0,0 +1,51 @@
+
+/* JavaScript for toggling list*/
+		function toggleDiv() {
+		var a = document.getElementById("drop1")
+		if (a.style.display == "block") {
+			a.style.display = "none";
+		}
+		else {
+			a.style.display = "block";
+		}
+	}
+
+	function toggleDiv2() {
+		var b = document.getElementById("drop2")
+		if (b.style.display == "block") {
+			b.style.display = "none";
+		}
+		else {
+			b.style.display = "block";
+		}
+	}
+
+	function toggleDiv3() {
+		var b = document.getElementById("drop3")
+		if (b.style.display == "block") {
+			b.style.display = "none";
+		}
+		else {
+			b.style.display = "block";
+		}
+	}
+
+	function toggleDiv4() {
+		var b = document.getElementById("drop4")
+		if (b.style.display == "block") {
+			b.style.display = "none";
+		}
+		else {
+			b.style.display = "block";
+		}
+	}
+
+	function toggleDiv5() {
+		var b = document.getElementById("drop5")
+		if (b.style.display == "block") {
+			b.style.display = "none";
+		}
+		else {
+			b.style.display = "block";
+		}
+	}
